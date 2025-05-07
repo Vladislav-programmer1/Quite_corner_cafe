00725d904ea60940bb9cc61649258da39c6a8ca0
Shchegolev <shegolevva2009@gmail.com>
2025-05-07 22:51:18 +0300

    ADDED: icons for actions

--- forms/registration.py
+++ forms/registration.py
@@ -36,7 +36,7 @@ def validate_not_text_values(form: FlaskForm, field: Field):
 class SignupForm(FlaskForm):
     name = StringField('Имя', validators=[DataRequired(), validate_text_values])
     surname = StringField("Фамилия", validators=[DataRequired(), validate_text_values])
-    sex = RadioField("Пол", choices=['М', 'Ж'], validators=[DataRequired()])
+    sex = RadioField("Пол", choices=['Мужской', 'Женский'], validators=[DataRequired()])
     email = EmailField('E-mail', validators=[Email(), DataRequired(), email_validation])
     phone_number = TelField('Номер телефона', validators=[DataRequired(), validate_not_text_values, phone_validation])
     password = PasswordField('Пароль', validators=[DataRequired(), ])

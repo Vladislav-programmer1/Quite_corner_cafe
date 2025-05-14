f4081e5f3a4f7e1e6df86e440aa8c034cae7a950
Shchegolev <shegolevva2009@gmail.com>
2025-05-14 07:49:14 +0300

    FIXED: how to save cart changes in the session, UPDATED: JS for menu.html

--- forms/authorizing.py
+++ forms/authorizing.py
@@ -1,10 +1,1 @@
-from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
 
-
-class LoginForm(FlaskForm):
-    email = EmailField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField("Войти")
